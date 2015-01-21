30b3e40366c225af0a30b30f4c5d5e600d0b908c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2015-01-21 15:48:41 +0100

    Use Windows API if available

--- src/bagger.cpp
+++ src/bagger.cpp
@@ -4,7 +4,12 @@
 #include <cmath>
 #include <iostream>
 #include <sstream>
+
+#ifdef __MINGW32__
+#include <windows.h>
+#else
 #include <sys/time.h>
+#endif // __MINGW32__
 
 #include "Bagger.h"
 
@@ -27,15 +32,11 @@ void Bagger::generateRandom()
     generate();
 }
 
-void Bagger::generate()
+inline void Bagger::generate()
 {
     this->m_StrdateNumber = cycle<int>(this->m_nDay,
-                                           this->m_nSec,
-                                           this->m_nNano);
-}
-
-Bagger::~Bagger()
-{
+                                       this->m_nSec,
+                                       this->m_nNano);
 }
 
 std::string Bagger::toString() const {
@@ -49,7 +50,7 @@ std::string Bagger::cycle(T nDay, T nSec, T nNanoSec)
     ldiv_t sDiv;
 	unsigned int i;
 	std::stringstream ss;
-	char timeName[12];
+	char timeName[13];
 
 	dTime = nSec * 1E9 + nNanoSec;
 	dTime = (dTime * (2821109907456)) / 86400E9;
@@ -66,17 +67,30 @@ std::string Bagger::cycle(T nDay, T nSec, T nNanoSec)
 		sDiv = ldiv(sDiv.quot, 36);
 		timeName[i] = sDiv.rem < 10 ? '0' + (char)sDiv.rem : 'a' + (char)sDiv.rem - 10;
 	}
+	timeName[12] = '\0';
     ss << timeName;
 	return ss.str();
 }
 
 void Bagger::getCurrentTime()
 {
+#ifdef __MINGW32__
+	FILETIME ft;
+	ULARGE_INTEGER ul;
+	GetSystemTimeAsFileTime(&ft);
+	ul.LowPart = ft.dwLowDateTime;
+	ul.HighPart = ft.dwHighDateTime;
+	this->m_nDay    = (int)(ul.QuadPart / 864000000000) - this->m_cnDays1601_1970;
+	this->m_nSec    = (int)((ul.QuadPart % 864000000000) / 10000000);
+	this->m_nNano   = (int)(ul.QuadPart % 10000000) * 100;
+#else
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
-	this->m_nDay = tv.tv_sec / 86400;
-	this->m_nSec = tv.tv_sec % 86400;
-	this->m_nNano = tv.tv_usec * 1000;
+
+	this->m_nDay    = tv.tv_sec / 86400;
+	this->m_nSec    = tv.tv_sec % 86400;
+	this->m_nNano   = tv.tv_usec * 1000;
+#endif
 }
 
 std::ostream& operator<<(std::ostream& out, Bagger& cBagger)
